dab5945f14d87f7108a1f7043373db489cccc2a2
althebug <alexe.montaudon@etu.univ-amu.com>
2021-11-04 21:39:05 +0100

    test EmptySide et ColoredSide

--- src/test/java/EmptySideTest.java
+++ src/test/java/EmptySideTest.java
@@ -1,9 +1,8 @@
+package sideTest;
+
 import javafx.scene.paint.Color;
 import model.EmptySide;
-import model.Side;
 import org.junit.jupiter.api.Test;
-
-import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
